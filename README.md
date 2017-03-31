1fb0173f7e3738bc0dc496024b231b9018e2b01d
Sarah Khalil <mkhalil.sarah@gmail.com>
2017-03-31 18:36:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+frontend-app
+============
+
+A Symfony project created on March 28, 2017, 10:34 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction()
+    {
+        return $this->render('default/admin.html.twig');
+    }
+
+    /**
+     * @Route("/admin/auth", name="admin_auth")
+     */
+    public function adminAuthAction()
+    {
+        // To avoid the ?code= url. Can be done with Javascript.
+        return $this->redirectToRoute('admin');
+    }
+
+
+    /**
+     * @Route("/admin/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+    }
+}
